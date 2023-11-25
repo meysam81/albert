8f0f564729bc0cc5227a8f7ec8523d9e6b4b4942
Meysam Azad <MeysamAzad81@gmail.com>
2023-11-25 23:39:10 +0700

    feat: add basic arithmetic functionality

--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,79 @@
 //     - push the operand, push everything remaining in the temp vector
 
 fn is_operand(elem: &str) -> bool {
-  elem.parse::<i32>().is_ok()
+    elem.parse::<i32>().is_ok()
 }
 
-fn is_higher_prio(coming: &str, current: &str) -> bool {
+fn is_current_higher_prio(current: &str, coming: &str) -> bool {
     let high: Vec<&str> = vec!["*", "/"];
     let low: Vec<&str> = vec!["-", "+"];
 
-    high.contains(&coming) && low.contains(&current)
+    high.contains(&current) || (low.contains(&current) && low.contains(&coming))
+}
+
+fn do_calculate(operand1: &i32, operator: &str, operand2: &i32) -> i32 {
+    match operator {
+        "*" => operand1 * operand2,
+        "/" => operand1 / operand2,
+        "+" => operand1 + operand2,
+        "-" => operand1 - operand2,
+        &_ => unimplemented!(),
+    }
 }
 
 fn infix_to_prefix(args: Vec<String>) -> Vec<String> {
     let mut stack: Vec<String> = vec![];
-    let operand = ["+", "-", "/", "*"];
-    let _ = args
-        .into_iter()
-        .map(|elem| {
-          let mut stack: Vec<String> = vec![];
-          if is_operand(&elem) {
-            stack.push(elem);
-          } else {
-            let mut partial: Vec<String> = vec![];
-            partial.push(stack.pop().unwrap());
+    let mut output: Vec<String> = vec![];
 
-            let _: Vec<_> = partial.into_iter().rev().map(|e| stack.push(e)).collect();
-          }
-        })
-        .collect::<Vec<_>>();
-    stack
+    args.into_iter().rev().for_each(|op| {
+        // println!("{:?} {:?} {:?}", &stack, &output, &op);
+        if is_operand(&op) {
+            output.push(op);
+        } else {
+            while stack.len() != 0 && !is_current_higher_prio(&op, stack.last().unwrap()) {
+                // println!("{:?} {:?}", &stack, &output);
+                output.push(stack.pop().unwrap());
+            }
+            stack.push(op);
+        }
+    });
+
+    stack.into_iter().for_each(|operand| output.push(operand));
+
+    output.into_iter().rev().collect()
+}
+
+fn calculate_result(prefix_notation: Vec<String>) -> i32 {
+    let mut stack: Vec<_> = vec![];
+    prefix_notation.into_iter().rev().for_each(|op| {
+        if is_operand(&op) {
+            stack.push(op);
+        } else {
+            let operand1 = stack.pop().unwrap().parse::<i32>().unwrap();
+            let operand2 = stack.pop().unwrap().parse::<i32>().unwrap();
+            stack.push(do_calculate(&operand1, &op, &operand2).to_string())
+        }
+    });
+
+    stack[0].parse::<i32>().unwrap()
 }
 
 fn main() {
     let cmd_line_args = std::env::args().skip(1).collect::<Vec<String>>();
     println!("{:?}", cmd_line_args);
-    println!("{:?}", infix_to_prefix(cmd_line_args));
+    println!("{:?}", calculate_result(infix_to_prefix(cmd_line_args)));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+  
+    #[test]
+    fn basic_arithmetic() {
+        // 3 + 4 * 5 / 6
+        // + 3 / * 4 5 6
+        let inp: Vec<String> = "3 + 4 * 5 / 6".split(" ").collect();
+        let rv = calculate_result(inp);
+        assert_eq!(rv, 6);
+    }
 }
